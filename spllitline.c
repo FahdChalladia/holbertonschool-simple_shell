2a5237aff20da8adf2cd08d922e7975ebed6c9c8
mac <mac@SK4DYs-MacBook.local>
2025-04-27 13:42:22 +0100

    v2 excution

--- spllitline.c
+++ spllitline.c
@@ -1,12 +1,46 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * lsh_split_line - Splits a command line string into an array of arguments.
- * @line: The raw input string entered by the user.
+ * lsh_split_line - Splits a line into tokens (commands and arguments)
+ * @line: The input string (command line)
  *
- * Return: An array of strings (tokens), ending with NULL.
+ * Return: An array of tokens (commands and arguments)
  */
 char **lsh_split_line(char *line)
 {
+int bufsize = 64, position = 0;
+char **tokens = malloc(bufsize * sizeof(char *));
+char *token;
 
+if (!tokens)
+{
+perror("split_line: allocation error");
+exit(EXIT_FAILURE);
+}
+
+token = strtok(line, " \t\r\n\a");
+while (token != NULL)
+{
+tokens[position] = token;
+position++;
+
+if (position >= bufsize)
+{
+bufsize += 64;
+tokens = realloc(tokens, bufsize *sizeof(char *));
+if (!tokens)
+{
+perror("split_line: allocation error");
+exit(EXIT_FAILURE);
+}
+}
+
+token = strtok(NULL, " \t\r\n\a");
+}
+
+tokens[position] = NULL;
+return (tokens);
 }
